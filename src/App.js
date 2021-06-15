8ef3c5f1a2db49e88b6f08a6dde610e452eebc95
FREEDOM <freewhynane62@gmail.com>
2021-06-15 13:17:22 +0000

    add all component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Navbar from './Component/Navbar'
+import Carousel from './Component/Carousel'
+import About from './Component/About'
+import PizzTypes from './Component/PizzaTypes'
 import './App.css';
 
 function App() {
   return (
     <>
-      
+      <Navbar/>
+      <Carousel/>
+      <About/>
+      <PizzTypes/>
     </>
   );
 }
