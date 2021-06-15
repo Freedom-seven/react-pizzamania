dc3e23836c34426323a39056dd84f804f2ccf602
FREEDOM <freewhynane62@gmail.com>
2021-06-15 14:44:34 +0000

    add code to footer component

---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div class="footer">
+                <div class="container">
+                    <div class="row">
+                        <p class="text-center">
+                            &copy; 2019 Pizzamania All Right Reserved!
+                    </p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Footer;
